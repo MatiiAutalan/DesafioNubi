0353defb51a9ade587789bda94e76a369b5c5081
MatiiAutalan <autalan.matiash@gmail.com>
2020-08-03 11:53:51 -0300

    se creo la carpeta service para almacenar la baseURL de la api dentro del archivo api.js y ademas un servicio para realizar las consultas a la api correspondiente

--- clima-app/src/App.js
+++ clima-app/src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
 import SearchBar from "./components/SearchBar";
-import WeatherData from "./components/WeatherData"
+import WeatherData from "./components/WeatherData";
+import { fetchWeather } from "./services/serviceWeather"
 
-function App() {
-  return (
-    <div className="App">
-      <SearchBar />
-      <WeatherData/>
-    </div>
-  );
+class App extends Component {
+
+  getWeather = async e => {
+    e.preventDefault()
+    const { city } = e.target.elements;
+    const cityValue = city.value;
+
+    const response = await fetchWeather(cityValue);
+    console.log(response.data);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <SearchBar getWeather={this.getWeather} />
+        <WeatherData />
+      </div>
+    );
+  }
 }
 
 export default App;
--- clima-app/src/components/SearchBar/index.js
+++ clima-app/src/components/SearchBar/index.js
@@ -3,14 +3,12 @@ import React from "react";
 const SearchBar = (props) => {
   return (
     <div className="container">
-      <form>
+      <form onSubmit={props.getWeather}>
         <div>
           <input type="text" name="city" placeholder="El tiempo en..." />
         </div>
+        <button>Buscar</button>
       </form>
-      <button>
-          Buscar
-      </button>
     </div>
   );
 };
